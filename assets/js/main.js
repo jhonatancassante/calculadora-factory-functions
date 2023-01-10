a3e368a20e74e225233e8046bc76e97eeef92c46
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-01-10 13:49:10 -0300

    Update calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,13 @@
     function criaCalculadora() {
         return {
             display: document.querySelector('.display'),
+            parenteses: 0,
+            ponto: false,
+            igual: false,
 
             inicia() {
                 this.cliqueBotoes();
-                this.pressionaEnter();
+                this.entradaTeclado();
             },
 
             cliqueBotoes() {
@@ -14,7 +17,7 @@
                     const el = e.target;
 
                     if (el.classList.contains('btn-num')) {
-                        this.btnParaDisplay(el.innerText);
+                        this.paraDisplay(el.innerText);
                     }
 
                     if (el.classList.contains('btn-clear')) {
@@ -28,40 +31,167 @@
                     if (el.classList.contains('btn-eq')) {
                         this.realizaConta();
                     }
-                });
-            },
 
-            pressionaEnter() {
-                document.addEventListener('keypress', (e) => {
-                    if (e.key === 'Enter') {
-                        this.realizaConta();
+                    if (el.classList.contains('btn-operator')) {
+                        this.operadorDisplay(el.innerText);
+                    }
+
+                    if (el.classList.contains('btn-open')) {
+                        this.parentesesAberto(el.innerText);
+                    }
+
+                    if (el.classList.contains('btn-close') && this.parenteses) {
+                        this.parentesesFechado(el.innerText);
+                    }
+
+                    if (el.classList.contains('btn-dot')) {
+                        this.dotDisplay(el.innerText);
                     }
                 });
             },
 
-            btnParaDisplay(valor) {
-                this.display.value += valor;
+            entradaTeclado() {
+                this.display.addEventListener('keypress', (e) => {
+                    e.preventDefault();
+
+                    if (e.key === 'Escape' || e.key === "Delete") {
+                        this.clearDisplay();
+                        return;
+                    }
+
+                    if (e.key === 'Backspace') {
+                        this.apagaUm();
+                        return;
+                    }
+
+                    if (e.key === 'Enter') {
+                        this.realizaConta();
+                        return;
+                    }
+
+                    if (e.key === '/' || e.key === '*' || e.key === '-' || e.key === '+') {
+                        this.operadorDisplay(e.key);
+                        return;
+                    }
+
+                    if (e.key === '(') {
+                        this.parentesesAberto(e.key);
+                        return;
+                    }
+
+                    if (e.key === ')' && this.parenteses) {
+                        this.parentesesFechado(e.key);
+                        return;
+                    }
+
+                    if (e.key === '.') {
+                        this.dotDisplay(e.key);
+                        return;
+                    }
+
+                    this.paraDisplay(e.key);
+                });
+            },
+
+            paraDisplay(valor) {
+                if (this.igual) {
+                    this.display.value = '';
+                    this.igual = false;
+                }
+                if (this.testaConta(valor)) {
+                    this.display.value += valor;
+                }
+            },
+
+            parentesesAberto(valor) {
+                if (!this.ultimoENumero() && this.ponto) {
+                    return;
+                }
+                if (this.ultimoENumero() && !this.igual) {
+                    this.paraDisplay('*')
+                }
+                this.paraDisplay(valor);
+                this.parenteses++;
+            },
+
+            parentesesFechado(valor) {
+                if (this.ultimoENumero()) {
+                    this.paraDisplay(valor);
+                    this.parenteses--;
+                }
+            },
+
+            dotDisplay(valor) {
+                if (this.ultimoEParentesesFechado() || this.ponto) {
+                    return;
+                }
+                if (!this.ultimoENumero() || this.ultimoEParentesesAberto()) {
+                    this.paraDisplay('0')
+                }
+                this.paraDisplay(valor);
+                this.ponto = true;
+            },
+
+            operadorDisplay(valor) {
+                if (this.ultimoEParentesesAberto()) {
+                    this.display.value = this.display.value.slice(0, -1);
+                }
+                if (!this.ultimoENumero() && !this.ultimoEParentesesFechado()) {
+                    this.display.value = this.display.value.slice(0, -1);
+                }
+                this.paraDisplay(valor);
+                this.ponto = false;
+            },
+
+            ultimoENumero() {
+                if (this.display.value === '') {
+                    return false;
+                }
+                const lastDigit = Number(this.display.value.slice(-1));
+                return Number.isInteger(lastDigit);
+            },
+
+            ultimoEParentesesAberto() {
+                const lastDigit = this.display.value.slice(-1);
+                return lastDigit === '(';
+            },
+
+            ultimoEParentesesFechado() {
+                const lastDigit = this.display.value.slice(-1);
+                return lastDigit === ')';
             },
 
             clearDisplay() {
                 this.display.value = '';
+                this.ponto = false;
+                this.parenteses = 0;
+                this.igual = false;
             },
 
             apagaUm() {
+                const lastDigit = this.display.value.slice(-1);
+                if (lastDigit === '(') {
+                    this.parenteses--;
+                }
+                if (lastDigit === ')') {
+                    this.parenteses++;
+                }
+                if (lastDigit === '.') {
+                    this.ponto = false;
+                }
                 this.display.value = this.display.value.slice(0, -1);
             },
 
+            testaConta(conta) {
+                let pattern = /[0-9\.\-\/\+\*\(\)]+$/g;
+                return pattern.test(conta);
+            },
+
             realizaConta() {
-                let conta = this.display.value;
-
                 try {
-                    conta = eval(conta);
-
-                    if (!conta) {
-                        return;
-                    }
-
+                    const conta = eval(this.display.value);
                     this.display.value = conta;
+                    this.igual = true;
                 } catch (error) {
                     alert('Conta inválida!');
                     return;
